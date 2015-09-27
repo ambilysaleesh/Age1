aa5c5d900363987062f4d3405356a22e4105be1a
ambily <ambilysaleesh@gmail.com>
2015-09-27 16:08:01 +0100

    fixed error in showing pop up error

--- newage/app/src/main/java/com/sid/saleesh/newage/MainActivity.java
+++ newage/app/src/main/java/com/sid/saleesh/newage/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
