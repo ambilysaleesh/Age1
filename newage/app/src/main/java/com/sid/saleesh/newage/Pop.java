9411132b473006a03266191064ec40a26875ba9f
ambily <ambilysaleesh@gmail.com>
2015-09-27 15:02:13 +0100

    moved to a new directory due to package naming issue

---
+++ newage/app/src/main/java/com/sid/saleesh/newage/Pop.java
@@ -0,0 +1,43 @@
+package com.sid.saleesh.newage;
+
+/**
+ * Created by saleesh on 27/09/15.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+
+
+/**
+ * Created by ambily saleesh on 22/09/15.
+ */
+public class Pop extends Activity {
+    TextView popupMessage;
+
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popwindow);
+        popupMessage = (TextView)findViewById(R.id.text_message);
+        Button ok_button = (Button)findViewById(R.id.button_ok);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8), (int)(height*.1));
+        ok_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.e("Popup", "I m OK btn");
+                finish();
+            }
+
+        });
+    }
+}
