9d8679006b523c1da7914e69b3392cb1f574294e
ambily <ambilysaleesh@gmail.com>
2015-09-30 22:43:21 +0100

    new UI cheanges in the branch

--- newage/app/src/main/java/com/sid/saleesh/newage/MainActivity.java
+++ newage/app/src/main/java/com/sid/saleesh/newage/MainActivity.java
@@ -1,154 +1,98 @@
 package com.sid.saleesh.newage;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
 
+public class MainActivity extends ActionBarActivity  {
 
+    TextView textView_current;
+    TextView textView_dob;
+    TextView textView_result;
+    TextView textView_next;
 
+    Button btn_calculate;
+    ImageButton image_button;
+    static  final int DIALOG_ID = 0;
 
+    int start_day;
+    int start_month;
+    int start_year;
 
-/**
- *  main for Age Calculator
- **/
-
-public class MainActivity extends ActionBarActivity {
-
-    Spinner spinday ;
-    Spinner spinmonth;
-    Spinner spinyear;
-    Button btn_submit;
-    TextView text_today;
-    TextView text_result;
-    TextView text_next;
-
-
-    int res_year ;
+    int res_year;
     int res_month;
     int res_day;
+
+    int prev_month;
+    int prev_days;
 
     final Calendar c = Calendar.getInstance();
     int current_year = c.get(Calendar.YEAR);
     int current_month = c.get(Calendar.MONTH);
     int current_day = c.get(Calendar.DAY_OF_MONTH);
 
-    int start_day;
-    int start_month;
-    int start_year;
-
-    int yy;
-    int prev_month;
-    int prev_days;
-    int month;
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        spinday = (Spinner)findViewById(R.id.spinnerdate);
-        spinmonth = (Spinner)findViewById(R.id.spinnermonth);
-        spinyear = (Spinner)findViewById(R.id.spinneryear);
-        btn_submit = (Button)findViewById(R.id.button);
-        text_result = (TextView)findViewById(R.id.textResult);
-        text_next = (TextView)findViewById(R.id.textView_next);
 
+        textView_current = (TextView)findViewById(R.id.textView_current_id);
+        textView_dob= (TextView)findViewById(R.id.textView_dob_id);
+        textView_result = (TextView)findViewById(R.id.textView_result_id);
+        textView_next = (TextView)findViewById(R.id.textView_next_id);
 
-        //current_month starts from 0
-        current_month = current_month + 1;
+        textView_current.setText(new StringBuilder().append(current_day).append(" - ").append(current_month +1).append(" - ").append(current_year));
 
-        text_today = (TextView)findViewById(R.id.textCurrent_day);
-        text_today.setText("Today is : " + new StringBuilder().append(current_day).append("").append(" - ").append(current_month).append(" - ")
-                .append(current_year));
-        text_today.setTextColor(Color.BLUE);
+        showMydate();
 
-        ArrayAdapter<CharSequence> adapter_day = ArrayAdapter.createFromResource(this, R.array.date, android.R.layout.simple_spinner_item);
-        ArrayAdapter<CharSequence> adapter_month = ArrayAdapter.createFromResource(this, R.array.month, android.R.layout.simple_spinner_item);
-        ArrayAdapter<CharSequence> adapter_year = ArrayAdapter.createFromResource(this, R.array.year, android.R.layout.simple_spinner_item);
-
-        adapter_day.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinday.setAdapter(adapter_day);
-
-        adapter_month.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinmonth.setAdapter(adapter_month);
-
-        adapter_year.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinyear.setAdapter(adapter_year);
-
-        spinday.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        btn_calculate = (Button)findViewById(R.id.button_calculate);
+        btn_calculate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                // Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) + " day selected ", Toast.LENGTH_LONG).show();
-                String str_start_day = parent.getItemAtPosition(position).toString();
-                start_day = Integer.parseInt(str_start_day);
-            }
-
-            public void onNothingSelected(AdapterView<?> parent) {
-                start_day = 5;
-            }
-        });
-
-
-        spinmonth.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                //Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) + " month selected ", Toast.LENGTH_LONG).show();
-                String str_start_month = parent.getItemAtPosition(position).toString();
-                start_month = Integer.parseInt(str_start_month);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        spinyear.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                // Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) + " year selected ", Toast.LENGTH_LONG).show();
-                String str_start_year = parent.getItemAtPosition(position).toString();
-
-                start_year = Integer.parseInt(str_start_year );
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-
-        btn_submit.setOnClickListener(new View.OnClickListener() {
-
             public void onClick(View v) {
-                //Log.e("exp", "Clicked Calculate ");
 
-                text_result.setText("You are: ");
-                text_next.setText("Your next birthday will be on: ");
 
-                //1. Get current Year, month, day here
-                if(isValidInput() == false){
+                if(isValidInput() == false) {
                     //Log.e("Popup", "I m popup");
                     startPopUpWindow();
                     return;
                 }
 
-                calculateAge();
+                findMyAge();
+
             }
         });
+
+
+    }
+
+    public void showMydate(){
+        image_button = (ImageButton)findViewById(R.id.imageButton);
+
+        image_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DIALOG_ID);
+
+
+
+            }
+        });
+
     }
 
     public void startPopUpWindow(){
@@ -175,7 +119,7 @@ public class MainActivity extends ActionBarActivity {
 
 
     /* Main logic of calculating the age in this function */
-    public void calculateAge() {
+    public void findMyAge() {
         /*
         Log.e("Day", Integer.toString(start_day));
         Log.e("Month", Integer.toString(start_month));
@@ -218,14 +162,13 @@ public class MainActivity extends ActionBarActivity {
 
         }
 
-        text_result.append( Integer.toString(res_year) +" years "+Integer.toString(res_month)+" months " +Integer.toString(res_day)+" days old "  );
-        text_next.append(Integer.toString(start_day )+ "/" + Integer.toString(start_month ) + "/" + Integer.toString(current_year + 1 ));
-        text_result.setTextColor(Color.BLUE);
+        textView_result.setText(Integer.toString(res_year) + " years " + Integer.toString(res_month) + " months " + Integer.toString(res_day) + " days old ");
+        textView_next.setText(Integer.toString(start_day) + "/" + Integer.toString(start_month) + "/" + Integer.toString(current_year + 1));
+        //textView_result.setTextColor(Color.BLUE);
     } /* endfn calculateAge */
 
 
 
-    /* get number of days: TODO: needs to consider leap year in the future */
     public int get_days(int month){
         int days = 30;
 
@@ -237,6 +180,33 @@ public class MainActivity extends ActionBarActivity {
             days = 30;
         return (days);
     } /* endfn get_days() */
+
+
+
+
+
+    protected Dialog onCreateDialog(int id) {
+        return new DatePickerDialog(this, datePickerListener, current_year, current_month, current_day);
+    }
+
+    private DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+            start_day = selectedDay;
+            start_month = selectedMonth ;
+            start_year = selectedYear;
+
+            Log.e("start_day", Integer.toString(start_day));
+            Log.e("start_month", Integer.toString(start_month));
+            Log.e("start_year", Integer.toString(start_year));
+
+
+            textView_dob.setText(new StringBuilder().append(start_day).append(" - ").append(start_month +1).append(" - ").append(start_year));
+
+
+        }
+    };
+
 
 
 
